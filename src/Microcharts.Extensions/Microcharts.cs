6f41eee4ed82a55db2c838ef959b43f3b5e1a855
Kerry Jiang <kerry-jiang@hotmail.com>
2019-05-08 22:38:42 -0700

    first commit

---
+++ src/Microcharts.Extensions/Microcharts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microcharts;
+
+namespace Microcharts.Extensions
+{
+    public static class Microcharts
+    {
+        public static BarChart CreateBarChart(Entry[] entries)
+        {
+            return new BarChart { Entries = entries };      
+        }
+    }
+}
