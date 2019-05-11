c42f87e217635943aa8c7a19ba125a274f5a640e
Kerry Jiang <kerry-jiang@hotmail.com>
2019-05-10 23:11:54 -0700

    added test project

---
+++ test/Test/MainTest.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+using Microcharts.Extensions;
+
+namespace Test
+{
+    public class MainTest
+    {
+        private ITestOutputHelper _output;
+
+        public MainTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void TestBarChart()
+        {
+            _output.WriteLine(MicrochartsBuilder.CreateBarChart(new [] { "C#", "Python", "Java", "Javascript" }, new [] {10, 8, 7, 9}).Show().ToString());
+        }
+    }
+}
