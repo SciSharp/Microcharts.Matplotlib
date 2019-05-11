068aff83461b71d9eabcd2b90c676aae25b4ecd1
Kerry Jiang <kerry-jiang@hotmail.com>
2019-05-10 23:11:44 -0700

    return JObject as DisplayData

---
+++ src/Microcharts.Extensions/MicrochartsBuilder.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Microcharts;
+using SkiaSharp;
+using Newtonsoft.Json.Linq;
+
+namespace Microcharts.Extensions
+{
+    public static class MicrochartsBuilder
+    {
+        public static BarChart CreateBarChart(IList<string> labels, IList<int> values)
+        {
+            if (labels.Count !=  values.Count)
+                throw new Exception($"{nameof(labels)} and {nameof(values)} have different length.");
+            
+            var entries = Enumerable.Range(0, labels.Count).Select(i =>
+                new Entry(values[i])
+                {
+                    Label = labels[i]
+                });
+
+            return new BarChart { Entries = entries };
+        }
+
+        public static JObject Show(this Chart chart, int width = 500, int height = 300)
+        {
+            var imageData = new JArray();
+            
+            using (var bitmap = new SKBitmap(width, height))
+            using (var canvas = new SKCanvas(bitmap))
+            {
+                chart.Draw(canvas, width, height);
+                var pngImage = SKImage.FromBitmap(bitmap).Encode(SKEncodedImageFormat.Png, 100);
+
+                using (var ms = new MemoryStream())
+                {
+                    pngImage.SaveTo(ms);
+                    ms.Seek(0, SeekOrigin.Begin);
+
+                    var buffer = new byte[96];
+
+                    while (true)
+                    {
+                        var read = ms.Read(buffer, 0, buffer.Length);
+
+                        if (read > 0)
+                        {
+                            imageData.Add(Convert.ToBase64String(buffer, 0, read) + "\n");
+                        }
+
+                        if (read < buffer.Length)
+                            break;
+                    }
+                }
+            }
+
+            return new JObject {
+                {
+                    "metadata", new JObject {
+                       {
+                           "image/png", new JObject {
+                                { "width", width },
+                                { "height", height }
+                            }
+                       }
+                    }
+                },
+                {
+                    "data", new JObject {
+                        { "image/png", imageData }
+                    }
+                }
+            };
+
+            /* 
+
+            return new JObject
+            {
+                "metadata", new JObject {
+                    "image/png", new JObject {
+                        { "width", width },
+                        { "height", height }
+                    }
+                },
+                "data", new JObject {
+                    "image/png", imageData
+                }
+            }; 
+            */
+        }
+    }
+}
